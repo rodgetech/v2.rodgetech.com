80c205b8c32435000575ca6d109e305465e7aacc
Luis <rodgetech@gmail.com>
2025-12-14 19:31:36 -0600

    cleanup config

--- config/site.ts
+++ config/site.ts
@@ -18,22 +18,8 @@ export const USER = {
   email: "rodgetech@gmail.com",
   website: "https://rodgetech.com",
   jobTitle: "Software Engineer",
-
-  about: `
-- **Design Engineer** with **5+ years of experience**, known for pixel-perfect execution and strong attention to small details.
-- Skilled in **Next.js**, **React**, **TypeScript**, and modern front-end technologies; building high-quality, user-centric web and mobile applications.
-- Passionate about exploring new technologies and turning ideas into reality through polished, thoughtfully crafted personal projects.
-- Creator of [ZaDark](https://zadark.com) (2022): enhances the Zalo experience on PC & Web
-  - **80k+ downloads** on [SourceForge](https://sourceforge.net/projects/zadark)
-  - **30k+ active users** on the [Chrome Web Store](https://chromewebstore.google.com/detail/llfhpkkeljlgnjgkholeppfnepmjppob)
-- Creator of [React Wheel Picker](https://react-wheel-picker.chanhdai.com): iOS-like wheel picker with inertia scrolling & infinite loop
-  - **10k+ weekly downloads** on [npm](https://www.npmjs.com/package/@ncdai/react-wheel-picker)
-  - [▲Vercel OSS Program](https://vercel.com/blog/summer-2025-oss-program#react-wheel-picker) summer 2025 cohort
-`,
-  avatar: "https://assets.chanhdai.com/images/chanhdai-avatar-ghibli.webp",
-  ogImage:
-    "https://assets.chanhdai.com/images/screenshot-og-image-light.png?t=1764345394",
-  namePronunciationUrl: "/audio/chanhdai.mp3",
+  avatar: "/me.png",
+  ogImage: "/me.png",
   timeZone: "America/Belize",
   keywords: ["rodgetech", "luis rodriguez", "luis", "rodriguez"],
   dateCreated: "2025-12-14", // YYYY-MM-DD
