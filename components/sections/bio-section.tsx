9809d1c50e96538014a04e532339c1bd5f958b33
Luis <rodgetech@gmail.com>
2025-12-14 19:03:46 -0600

    minor change

--- components/sections/bio-section.tsx
+++ components/sections/bio-section.tsx
@@ -3,6 +3,8 @@ import { USER } from "@/config/site";
 export function BioSection() {
   return (
     <p className="leading-8 text-zinc-600 dark:text-zinc-400 whitespace-pre-line">
+      Hi internet stranger,
+      <br />
       {USER.longBio.trim()}
     </p>
   );
